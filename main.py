81bc1553a896afd32cb00f9fbdf335c70d1ac17c
KLPig <kkkkkent@qq.com>
2024-04-20 13:10:10 +0800

    Add attacks and wave systems, add the SOUL.

--- main.py
+++ main.py
@@ -1,39 +1,8 @@
 from underpython import *
 import os
+import random
 
 player = Player('Chara', 92, 99, 16, 19)
-flowery_ani = Animations((240, 240), 4, tpf=5)
-flowery_ani.add_animation('idle', [
-    'monsters.flowery.idle.1',
-    'monsters.flowery.idle.2',
-    'monsters.flowery.idle.3',
-    'monsters.flowery.idle.4',
-    'monsters.flowery.idle.5',
-    'monsters.flowery.idle.6',
-    'monsters.flowery.idle.7',
-    'monsters.flowery.idle.6',
-    'monsters.flowery.idle.5',
-    'monsters.flowery.idle.4',
-    'monsters.flowery.idle.3',
-    'monsters.flowery.idle.2',
-    'monsters.flowery.idle.1',
-                          ])
-flowery_ani.add_animation('hurt',
-                          [
-                              'monsters.flowery.idle.1',
-                              'monsters.flowery.idle.1',
-                              'monsters.flowery.idle.1',
-                              'monsters.flowery.idle.1'
-                          ], nxt='idle')
-flowery_ani.change_animation('idle')
-flowery_a = Monster(flowery_ani, 'floweryA', 500, 90, 5, ['damn', 'shit', 'm3'])
-
-
-@flowery_a.events
-def on_act(name: str) -> str | None:
-    if name == 'damn':
-        return 'Damn[endl]Shit[endl]M3'
-
 flowery_ani = Animations((640, 240), 4, tpf=5)
 flowery_ani.add_animation('idle', [
     'monsters.flowery.idle.1',
@@ -58,38 +27,53 @@ flowery_ani.add_animation('hurt',
                               'monsters.flowery.idle.1'
                           ], nxt='idle')
 flowery_ani.change_animation('idle')
-flowery_b = Monster(flowery_ani, 'floweryB', 500, 90, 5)
-flowery_ani = Animations((1040, 240), 4, tpf=5)
-flowery_ani.add_animation('idle', [
-    'monsters.flowery.idle.1',
-    'monsters.flowery.idle.2',
-    'monsters.flowery.idle.3',
-    'monsters.flowery.idle.4',
-    'monsters.flowery.idle.5',
-    'monsters.flowery.idle.6',
-    'monsters.flowery.idle.7',
-    'monsters.flowery.idle.6',
-    'monsters.flowery.idle.5',
-    'monsters.flowery.idle.4',
-    'monsters.flowery.idle.3',
-    'monsters.flowery.idle.2',
-    'monsters.flowery.idle.1',
-                          ])
-flowery_ani.add_animation('hurt',
-                          [
-                              'monsters.flowery.idle.1',
-                              'monsters.flowery.idle.1',
-                              'monsters.flowery.idle.1',
-                              'monsters.flowery.idle.1'
-                          ], nxt='idle')
-flowery_ani.change_animation('idle')
-flowery_c = Monster(flowery_ani, 'floweryC', 500, 90, 5)
+flowery = Monster(flowery_ani, 'flowery', 500, 12, 5,
+                  ['heal'])
 
-monsters = [flowery_a, flowery_b, flowery_c]
+
+@flowery.events
+def on_act(name: str) -> str | None:
+    if name == 'check':
+        return 'Flowery, 5 df 12 at.[endl]Last enemy.'
+    elif name == 'heal':
+        GAME.player.heal(50)
+        return 'Recovered 50 HP!'
+
+
+monsters = [flowery]
+
+
+class Fb(Attack):
+    def u_ani(self):
+        self.set_img(GAME.graphics['pj.attack.fb.idle.%d' % (self.tick // 2 % 2 + 1)])
+
+
+class FbRise(Fb):
+    def on_action(self):
+        self.u_ani()
+        if self.tick > 40:
+            self.move_forward(40)
+        elif self.tick == 40:
+            self.face_to(GAME.ui.souls.get_now().pos)
+        else:
+            self.move_pos((0, -20))
 
 
 class Bullet(Wave):
-    pass
+    def on_wave_start(self):
+        game.GAME.ui.soul_rect.exp_rect.__init__(440, 300, 400, 400)
+        self.attacks.append(attacks.Attacks(GAME.monsters[0]))
+        GAME.ui.souls.get_now().set_pos(GAME.ui.soul_rect.rect.center)
+
+    def on_wave_update(self):
+        for atk in self.attacks:
+            atk.update()
+        if self.tick % 10 == 1:
+            self.attacks[0].add(FbRise((random.randint(0, 200), random.randint(1000, 1500)), game.GAME.graphics['pj.attack.fb.idle.1']))
+        elif self.tick % 10 == 6:
+            self.attacks[0].add(FbRise((random.randint(1080, 1280), random.randint(1000, 1500)), game.GAME.graphics['pj.attack.fb.idle.1']))
+        if self.tick >= 200:
+            self.end_wave()
 
 
 waves = [Bullet]
--- underpython/attacks.py
+++ underpython/attacks.py
@@ -1,39 +1,108 @@
-import copy
-
 from underpython import base
 import pygame as pg
 import math
+from underpython import game
 
 
 class Attack:
-    def events(self, func):
-        setattr(self, func.__name__, func)
+    enable_ul = True
 
-    def on_action(self, tick: int) -> bool: pass
+    def calc_dmg(self, attacker):
+        return max(0, attacker.at * 5 - game.GAME.player.df * 3)
+
+    def on_action(self): pass
 
     def on_init(self): pass
 
     def on_remove(self): pass
 
-    def __init__(self, position: tuple[int, int], damage: int | base.Constant = base.CALCULATE_DAMAGE, rotation=0): pass
+    def __init__(self, position: tuple[int, int], graphic: pg.surface.Surface, rotation=0, **kwargs):
+        self.pos = position
+        self.remove = False
+        self.arr = pg.PixelArray(graphic)
+        self.org = pg.transform.scale_by(graphic, 3)
+        self.rot = 0
+        self.dis: pg.surface.Surface = self.org
+        self.rect = self.org.get_rect()
+        self.attr = {}
+        self.tick = 0
+        for k, v in kwargs.items():
+            self.attr[k] = v
+        self.set_rotation(rotation)
 
-    def update_shapes(self): pass
+    def collide_point(self, point: tuple[int, int]) -> bool:
+        self.arr = pg.PixelArray(self.dis)
+        x, y = point
+        px, py = self.rect.topleft
+        if self.rect.collidepoint(x, y):
+            return self.arr[x - px, y - py]
+        else:
+            return False
 
-    def collide_point(self, point: tuple[int, int]): pass
+    def pos_to(self, steps: float = 1, rotation: float = None) -> tuple[float, float]:
+        rot = rotation
+        if rot is None:
+            rot = self.rot
+        ax = math.sin(math.radians(rot)) * steps
+        ay = math.cos(math.radians(rot)) * steps
+        return ax, -ay
 
-    def update(self): pass
+    def move_forward(self, steps: float = 1, rotation: float = None):
+        ax, ay = self.pos_to(steps, rotation)
+        self.move_pos((ax, ay))
 
-    def move_forward(self, steps: int = 1): pass
+    def move_pos(self, apos: tuple[float, float]):
+        ax, ay = apos
+        x, y = self.pos
+        self.set_pos((x + ax, y + ay))
 
-    def face_to(self, position: tuple[int, int]): pass
+    def angle_to(self, pos: tuple[float, float]) -> float:
+        dx, dy = pos
+        x, y = self.pos
+        ax, ay = dx - x, dy - y
+        if not ay:
+            return 270 - 180 * (ax > 0)
+        return math.degrees(math.atan(-ax / ay)) + 180 * (dy > y)
 
-    def set_rotation(self, rotation: int): pass
+    def face_to(self, pos: tuple[float, float]):
+        r = self.angle_to(pos)
+        self.set_rotation(r)
 
-    def set_attribute(self, name: str, data): pass
+    def set_pos(self, pos: tuple[float, float]):
+        self.pos = pos
+        x, y = self.pos
+        self.rect.center = int(x), int(y)
 
-    def get_attribute(self, name: str): pass
+    def set_rotation(self, rotation: float):
+        self.rot = (rotation % 360 + 360) % 360
+        self.dis = pg.transform.rotate(self.org, self.rot)
+        self.rect = self.dis.get_rect()
+        x, y = self.pos
+        self.rect.center = int(x), int(y)
 
-    def __index__(self, name: str): pass
+    def set_img(self, img: pg.Surface):
+        self.org = pg.transform.scale_by(img, 3)
+        self.set_rotation(self.rot)
+
+    def rotate(self, degree: float):
+        self.set_rotation(self.rot + degree)
+
+    def set_attribute(self, **kwargs):
+        for k, v in kwargs.items():
+            self.attr[k] = v
+
+    def get_attribute(self, **args):
+        tmp = []
+        for k in args.items():
+            if k in self.attr.keys():
+                tmp.append(self.attr[k[0]])
+            else:
+                raise base.UnderPythonError('Invalid key while getting attribute',
+                                            [self, k, self.get_attribute])
+        return tmp
+
+    def remove_atk(self):
+        self.remove = True
 
 
 class Attacks:
@@ -46,13 +115,34 @@ class Attacks:
 
     def on_update(self): pass
 
-    def __init__(self, attacker, targets: list): pass
+    def __init__(self, attacker):
+        self.attacker = attacker
+        self.attacks: list[Attack] = []
 
-    def change_targets(self: list): pass
+    def add(self, atk: Attack):
+        atk.on_init()
+        self.on_attack_added(atk)
+        self.attacks.append(atk)
 
-    def __add__(self, atk: Attack): pass
+    def __getitem__(self, item: int):
+        return self.attacks[item]
 
-    def __index__(self, idx: int): pass
+    def update(self):
+        self.on_update()
+        for atk in self.attacks:
+            if atk.remove:
+                atk.on_remove()
+                self.attacks.remove(atk)
+                self.on_attack_removed(atk)
+                continue
+            if atk.collide_point(game.GAME.ui.souls.get_now().pos) and not game.GAME.player.wd:
+                game.GAME.player.hurt(atk.calc_dmg(self.attacker))
+                if atk.enable_ul:
+                    game.GAME.player.st_wd()
+            atk.on_action()
+            atk.tick += 1
+            atk.dis.unlock()
+            game.GAME.displayer().blit(atk.dis, atk.rect)
 
 
 class SoulRect:
@@ -81,3 +171,92 @@ class SoulRect:
         else:
             self.rect.height += (self.exp_rect.height - self.rect.height) // 2
 
+
+class Soul:
+    color = (255, 255, 255)
+    move_in_rect = True
+
+    def __init__(self):
+        self.pos = (0, 0)
+        soul = [
+            [0, 1, 1, 0, 0, 1, 1, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [0, 1, 1, 1, 1, 1, 1, 0],
+            [0, 0, 1, 1, 1, 1, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0, 0]
+        ]
+        arr = pg.PixelArray(pg.Surface((8, 8)))
+        for i in range(8):
+            for j in range(8):
+                if soul[i][j]:
+                    arr[j, i] = self.color
+                else:
+                    arr[j, i] = 0
+        self.surface = pg.transform.scale_by(arr.make_surface(), 7)
+        arr = pg.PixelArray(pg.Surface((8, 8)))
+        for i in range(8):
+            for j in range(8):
+                if soul[i][j]:
+                    arr[j, i] = (255, 255, 255)
+                else:
+                    arr[j, i] = 0
+        self.unabled = pg.transform.scale_by(arr.make_surface(), 7)
+        self.rect = self.surface.get_rect()
+        x, y = self.pos
+        self.rect.center = int(x), int(y)
+
+    def set_pos(self, pos: tuple[float, float]):
+        s = game.GAME.ui.soul_rect.rect
+        x, y = pos
+        if self.move_in_rect:
+            x = max(min(s.right - 37, x), s.left + 37)
+            y = max(s.top + 37, min(s.bottom - 37, y))
+        self.pos = x, y
+        self.rect.center = int(x), int(y)
+
+    def move_pos(self, apos: tuple[float, float]):
+        ax, ay = apos
+        x, y = self.pos
+        self.set_pos((x + ax, y + ay))
+
+    def update(self):
+        pass
+
+
+class RedSoul(Soul):
+    color = (255, 0, 0)
+
+    def update(self):
+        if game.GAME.player.wd:
+            game.GAME.displayer().blit(self.unabled, self.rect)
+        else:
+            game.GAME.displayer().blit(self.surface, self.rect)
+        keys = pg.key.get_pressed()
+        spd = 10
+        if keys[pg.K_z]:
+            spd = 15
+        elif keys[pg.K_x]:
+            spd = 5
+        if keys[pg.K_UP]:
+            self.move_pos((0, -spd))
+        elif keys[pg.K_DOWN]:
+            self.move_pos((0, spd))
+        if keys[pg.K_LEFT]:
+            self.move_pos((-spd, 0))
+        elif keys[pg.K_RIGHT]:
+            self.move_pos((spd, 0))
+
+
+class Souls:
+    def __init__(self):
+        self.souls = [RedSoul()]
+        self.now = 0
+
+    def get_now(self) -> RedSoul | Soul:
+        return self.souls[self.now]
+
+    def set_now(self, now):
+        self.now = max(0, min(len(self.souls) - 1, now))
